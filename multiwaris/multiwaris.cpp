36a152da3431cfba4f49f2264d706958d7cc869e
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-06-02 18:09:08 +0700

    kelas pekerja mewarisi orang secara virtual

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -13,3 +13,12 @@ public:
         }
 };
 
+class pekerja : virtual public orang {
+public :
+    pekerja(int pUmur):
+        orang(pUmur)
+        {
+            cout << "pekerja dibuat\n" << endl;
+        }
+};
+
